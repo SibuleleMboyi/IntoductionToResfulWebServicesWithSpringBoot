de73f452901c4352908e66b31a7dc7bca0ee968a
SibuleleMboyi <smboyi2016@gmail.com>
2022-06-22 15:11:32 +0200

    internationalizing restful services

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +12,8 @@ import com.in28minutes.rest.webservices.restfulwebservices.models.HelloWorldBean
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "hello-world")
     public String hellowWorld() {
@@ -32,4 +37,24 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
+    // Internationalization( refered as i18n(because of it character length) ).
+    // It helps to customize services for different consumers around the world.
+    // It customizes services across different languages.
+    // Create a "properties" file to store customized information.
+    // name it "messages.properties" for English.
+    // name another one "messages_fr.properties" for French
+    // (google other naming conventions).
+    // After running the App,
+    // select a GET request and enter this end point URL,
+    // select hearders and add, "Accept-Language",
+    // then enter abbreviation e.g nl, fr, eng from these file
+    // names(messages_fr.properties).
+    // Send the request.
+    @GetMapping(path = "hello-world-internationalized")
+    public String hellowWorldInternationalized() {
+
+        return messageSource.getMessage("good.morning.message", null,
+                "Default Message",
+                LocaleContextHolder.getLocale());
+    }
 }
